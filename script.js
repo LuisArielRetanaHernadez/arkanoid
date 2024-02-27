daada38fa47181755ef57d6df41d707b5b45dc9f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-26 18:20:16 -0600

    creando las funciones para las funcionalidades del juego

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
 
+const dramBall = () => {}
+const dramPaddle = () => {}
+const dramBricks = () => {}
+
+const collisionDetection = () => {}
+const ballMovement = () => {}
+const paddleMovement = () => {}
+
+const dram = () => {
+
+  // hay que dibujar los elementos
+  dramBall()
+  dramPaddle()
+  dramBricks()
+
+  // colisiones y movimientos
+  collisionDetection()
+  ballMovement()
+  paddleMovement()
+  
+  window.requestAnimationFrame(dram);
+}
